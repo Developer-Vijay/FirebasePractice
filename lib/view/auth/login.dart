e1bfabcae936c539724e5f23fbf736642cd0a262
Developer-Vijay <vijaysahoo147@gmail.com>
2022-04-12 16:29:30 +0530

    Firebase Methods Making Learned,signInMethhod has Error

--- lib/view/auth/login.dart
+++ lib/view/auth/login.dart
@@ -1,14 +1,16 @@
 import 'package:firebase_practice/constants/app_colors.dart';
 import 'package:firebase_practice/constants/app_size_widget.dart';
+import 'package:firebase_practice/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
 
-class Login extends StatelessWidget {
+class Login extends ConsumerWidget {
   const Login({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    TextEditingController _numberController = TextEditingController();
 
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    AuthService _auth = AuthService();
     return SafeArea(
       child: Scaffold(
         body: SingleChildScrollView(
@@ -51,9 +53,7 @@ class Login extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.symmetric(
                       horizontal: 29.0, vertical: 10),
-                  child: Form(
-                    child: _numberField(),
-                  ),
+                  child: Form(child: _numberField()),
                 ),
                 SizedBox(
                   height: AppSizeConfig.heightMultiplier * 1,
@@ -73,6 +73,40 @@ class Login extends StatelessWidget {
                     textColor: AppColors.whitextColor,
                   ),
                 ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MaterialButton(
+                        onPressed: () async {
+                          dynamic result =
+                              await _auth.signInWithEmailPasswd(
+                                  "vijaysahoo147@gmail.com", "23432");
+
+                          if (result == null) {
+                            print("failed");
+                          } else {
+                            print("Success");
+                            print(result.uid);
+                          }
+                        },
+                        child: Icon(Icons.email),
+                        textColor: AppColors.whitextColor,
+                        color: AppColors.secondaryColor,
+                      ),
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: MaterialButton(
+                        onPressed: () {},
+                        child: Text("Signed in Annoysmously"),
+                        textColor: AppColors.whitextColor,
+                        color: AppColors.secondaryColor,
+                      ),
+                    ),
+                  ],
+                ),
                 Align(
                     alignment: Alignment.bottomCenter,
                     heightFactor: 1.1,
@@ -91,48 +125,87 @@ class Login extends StatelessWidget {
       ),
     );
   }
+}
 
-  TextFormField _numberField() {
-    return TextFormField(
+TextFormField _emailField() {
+  return TextFormField(
       style: const TextStyle(
         color: AppColors.secondaryColor,
       ),
       cursorColor: AppColors.secondaryColor,
       enabled: true,
-      keyboardType: TextInputType.number,
+      keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
         prefixIcon: const Padding(
-          padding: EdgeInsets.only(
-            left: 12,
-            bottom: 2,
-            right: 4,
-          ),
-          child: Text(
-            "+91",
-            style: TextStyle(
-              color: AppColors.secondaryColor,
+            padding: EdgeInsets.only(
+              left: 12,
+              bottom: 2,
+              right: 4,
             ),
+            child: Icon(Icons.email)),
+      ));
+}
+
+TextFormField _PasswordField() {
+  return TextFormField(
+      style: const TextStyle(
+        color: AppColors.secondaryColor,
+      ),
+      cursorColor: AppColors.secondaryColor,
+      enabled: true,
+      keyboardType: TextInputType.emailAddress,
+      obscureText: true,
+      decoration: InputDecoration(
+        prefixIcon: const Padding(
+            padding: EdgeInsets.only(
+              left: 12,
+              bottom: 2,
+              right: 4,
+            ),
+            child: Icon(Icons.email)),
+      ));
+}
+
+TextFormField _numberField() {
+  return TextFormField(
+    style: const TextStyle(
+      color: AppColors.secondaryColor,
+    ),
+    cursorColor: AppColors.secondaryColor,
+    enabled: true,
+    keyboardType: TextInputType.number,
+    decoration: InputDecoration(
+      prefixIcon: const Padding(
+        padding: EdgeInsets.only(
+          left: 12,
+          bottom: 2,
+          right: 4,
+        ),
+        child: Text(
+          "+91",
+          style: TextStyle(
+            color: AppColors.secondaryColor,
           ),
         ),
-        prefixIconConstraints: const BoxConstraints(
-          minWidth: 0,
-          minHeight: 0,
-        ),
-        hintText: " 00000000000",
-        enabled: true,
-        focusColor: AppColors.secondaryColor,
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: const BorderSide(
-              color: AppColors.secondaryColor, width: 1.3),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12),
-          borderSide: const BorderSide(
-              color: AppColors.secondaryColor, width: 1.3),
-        ),
-        hintStyle: const TextStyle(color: AppColors.secondaryColor),
       ),
-    );
-  }
+      prefixIconConstraints: const BoxConstraints(
+        minWidth: 0,
+        minHeight: 0,
+      ),
+      hintText: " 00000000000",
+      enabled: true,
+      focusColor: AppColors.secondaryColor,
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: const BorderSide(
+            color: AppColors.secondaryColor, width: 1.3),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: const BorderSide(
+            color: AppColors.secondaryColor, width: 1.3),
+      ),
+      hintStyle: const TextStyle(color: AppColors.secondaryColor),
+    ),
+  );
 }
--- lib/view/auth/signup.dart
+++ lib/view/auth/signup.dart
@@ -1,35 +1,96 @@
+import 'package:firebase_practice/Widgets/buttons.dart';
+import 'package:firebase_practice/constants/app_size_widget.dart';
 import 'package:firebase_practice/services/auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SignUp extends StatefulWidget {
+import '../../constants/app_colors.dart';
+
+AuthService _auth = AuthService();
+
+class SignUp extends ConsumerWidget {
   const SignUp({Key? key}) : super(key: key);
 
   @override
-  State<SignUp> createState() => _SignUpState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController =
+        TextEditingController();
 
-class _SignUpState extends State<SignUp> {
-  final AuthService _auth = AuthService();
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.brown[100],
-        appBar: AppBar(),
-        body: Container(
-          padding: const EdgeInsets.symmetric(
-              vertical: 20, horizontal: 50.0),
-          child: MaterialButton(
-            child: const Text("Sign in Annoysmously"),
-            onPressed: (() async {
-              dynamic result = await _auth.signInAnon();
-              if (result == null) {
-                print("Error Sign In");
-              } else {
-                print("success Fully SignIn");
-                print(result.uid);
-              }
-            }),
-          ),
-        ));
+      appBar: AppBar(
+        title: const Text("SignUp"),
+      ),
+      body: Column(children: [
+        SizedBox(
+          height: AppSizeConfig.heightMultiplier * 4,
+        ),
+        const Text("Register"),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(child: emailField(_emailController)),
+        ),
+        SizedBox(
+          height: AppSizeConfig.heightMultiplier * 4,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(child: passwordField(_passwordController)),
+        ),
+        SizedBox(
+          height: AppSizeConfig.heightMultiplier * 4,
+        ),
+        Buttons(
+            name: "SigupUsingEmailAndPassword",
+            textColor: AppColors.whitextColor,
+            onpress: _auth.registerWithEmailAndPassword(
+                _emailController, _passwordController),
+            buttoncolor: AppColors.secondaryColor),
+      ]),
+    );
+  }
+
+  TextFormField emailField(TextEditingController _emailController) {
+    return TextFormField(
+      controller: _emailController,
+      decoration: InputDecoration(
+        label: const Text("Email"),
+        prefixIcon: const Icon(Icons.email),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(
+              color: AppColors.secondaryColor, width: 1.3),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(
+              color: AppColors.secondaryColor, width: 1.3),
+        ),
+        contentPadding: const EdgeInsets.all(20),
+      ),
+    );
+  }
+
+  TextFormField passwordField(
+      TextEditingController _passwordController) {
+    return TextFormField(
+      controller: _passwordController,
+      decoration: InputDecoration(
+        label: const Text("Password"),
+        prefixIcon: const Icon(Icons.lock),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(
+              color: AppColors.secondaryColor, width: 1.3),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(
+              color: AppColors.secondaryColor, width: 1.3),
+        ),
+        contentPadding: const EdgeInsets.all(20),
+      ),
+    );
   }
 }
